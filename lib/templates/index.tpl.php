f206e477ff5b125ca6bc0caf0286925c20061203
Jurgens du Toit <jrgns@jrgns.net>
2011-10-24 17:50:26 +0200

    Render exception

--- lib/templates/index.tpl.php
+++ lib/templates/index.tpl.php
@@ -1,7 +1,14 @@
-<!DOCTYPE HTML>
+<?php
+$result = $this->_view->get('result');
+if ($result instanceof Exception) {
+    $title = 'Result: Exception';
+} else {
+    $title  = get_class($this->_view->get('modelObj'));
+}
+?><!DOCTYPE HTML>
 <html>
     <head>
-        <title>HtmlView - <?php echo get_class($this->_view->get('modelObj')) ?></title>
+        <title>HtmlView - <?php echo $title ?></title>
             <link rel="stylesheet" href="#SITE_SUB_FOLDER#css/blueprint/screen.css"
                 type="text/css" media="screen, projection">
             <link rel="stylesheet" href="#SITE_SUB_FOLDER#css/blueprint/print.css"
@@ -13,8 +20,39 @@
         </head>
     <body>
         <div class="container">
-            <h3>Result</h3>
-            <?php var_dump($this->_view->get('result')) ?>
+            <?php if ($result instanceof Exception): ?>
+                <h2>Result: Exception</h2>
+                <div class="error large">
+                    <h3>
+                        <?php echo $result->getMessage() ?>
+                    </h3>
+                    <label class="span-3">Code</label>
+                    <?php echo $result->getCode() ?><br>
+                    <label class="span-3">File</label>
+                    <?php echo $result->getFile() ?><br>
+                    <label class="span-3">Line</label>
+                    <?php echo $result->getLine() ?><br>
+                </div>
+                <div class="notice">
+                    <h3>Stack Trace</h3>
+                    <ol class="bottom">
+                        <?php foreach ($result->getTrace() as $line): ?>
+                            <li>
+                                <?php echo $line['file'] . '(' . $line['line'] . ')' ?>
+                            </li>
+                        <?php endforeach; ?>
+                    </ol>
+                </div>
+                <?php if (!empty($result->xdebug_message)): ?>
+                    <div class="notice">
+                        <h3>XDebug</h3>
+                        <table><?php echo $result->xdebug_message ?></table>
+                    </div>
+                <?php endif; ?>
+            <?php else: ?>
+                <h2>Result</h2>
+                <?php var_dump($this->_view->get('result')) ?>
+            <?php endif; ?>
             <div class="notice" id="buffered">
                 #buffered#
             </div>
