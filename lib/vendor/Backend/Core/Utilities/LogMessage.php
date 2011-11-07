5437210f636a9b233a9aad52f14b5b682331c41f
Jurgens du Toit <jrgns@jrgns.net>
2011-11-07 07:09:25 +0200

    Implemented Observable and LogMessage

---
+++ lib/vendor/Backend/Core/Utilities/LogMessage.php
@@ -0,0 +1,110 @@
+<?php
+namespace Backend\Core\Utilities;
+/**
+ * File defining LogMessage
+ *
+ * Copyright (c) 2011 JadeIT cc
+ * @license http://www.opensource.org/licenses/mit-license.php
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+ * A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @package UtilityFiles
+ */
+/**
+ * LogMessage
+ *
+ * Log levels:
+ * 1. Critical Messages
+ * 2. Warning | Alert Messages
+ * 3. Important Messages
+ * 4. Debugging Messages
+ * 5. Informative Messages
+ * @package Utilities
+ */
+class LogMessage implements \SplSubject
+{
+    const LEVEL_CRITICAL    = 1;
+    const LEVEL_WARNING     = 2;
+    const LEVEL_IMPORTANT   = 3;
+    const LEVEL_DEBUGGING   = 4;
+    const LEVEL_INFORMATION = 5;
+
+    protected $_observers = array();
+
+    protected $_level;
+
+    protected $_message;
+
+    public function __construct($message, $level)
+    {
+        switch ($level) {
+        case LogMessage::LEVEL_CRITICAL:
+            $message = ' (CRITICAL) ' . $message;
+            break;
+        case LogMessage::LEVEL_WARNING:
+            $message = ' (WARNING) ' . $message;
+            break;
+        case LogMessage::LEVEL_IMPORTANT:
+            $message = ' (IMPORTANT) ' . $message;
+            break;
+        case LogMessage::LEVEL_DEBUGGING:
+            $message = ' (DEBUG) ' . $message;
+            break;
+        case LogMessage::LEVEL_IMPORTANT:
+            $message = ' (INFORMATION) ' . $message;
+            break;
+        default:
+            $message = ' (OTHER - ' . $level . ') ' . $message;
+            break;
+        }
+
+        $this->_message = $message;
+
+        $this->_level   = $level;
+
+        Observable::execute($this);
+
+        $this->notify();
+    }
+
+    //SplSubject functions
+
+    public function attach(\SplObserver $observer)
+    {
+        $id = spl_object_hash($observer);
+        $this->_observers[$id] = $observer;
+    }
+
+    public function detach(\SplObserver $observer)
+    {
+        $id = spl_object_hash($observer);
+        unset($this->_observers[$id]);
+    }
+
+    public function notify()
+    {
+        foreach ($this->_observers as $obs) {
+            $obs->update($this);
+        }
+    }
+
+    function __toString()
+    {
+        return $this->_message;
+    }
+}
---
+++ lib/vendor/Backend/Core/Utilities/Observable.php
@@ -0,0 +1,59 @@
+<?php
+namespace Backend\Core\Utilities;
+/**
+ * File defining Observable
+ *
+ * Copyright (c) 2011 JadeIT cc
+ * @license http://www.opensource.org/licenses/mit-license.php
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+ * A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @package UtilityFiles
+ */
+/**
+ * Class to automatically attach observers to subjects
+ *
+ * @package Utilities
+ */
+class Observable
+{
+    /**
+     * Check for observers for the given subject
+     *
+     * An observer is only eligible if it implements \SplObserver, and can be retrieved
+     * using \Backend\Core\getTool
+     */
+    public static function execute(\SplSubject $subject)
+    {
+        $config = \Backend\Core\Application::getTool('Config');
+        if (!$config) {
+            return false;
+        }
+        //Attach Observers to Subjects
+        $config = $config->get('subjects', get_class($subject));
+        if (!empty($config->observers)) {
+            foreach ($config->observers as $observerName) {
+                $observer = \Backend\Core\Application::getTool($observerName);
+                if ($observer instanceof \SplObserver) {
+                    $subject->attach($observer);
+                }
+            }
+        }
+        return true;
+    }
+}
