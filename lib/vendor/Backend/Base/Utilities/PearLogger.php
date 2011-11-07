25d47e7e059c81a865ef6cef9d61e46f63b54392
Jurgens du Toit <jrgns@jrgns.net>
2011-11-07 08:15:18 +0200

    Added support for using the PEAR Logger

--- lib/vendor/Backend/Base/Utilities/PearLogger.php
+++ lib/vendor/Backend/Base/Utilities/PearLogger.php
@@ -13,12 +13,24 @@ class PearLogger implements \Backend\Core\Interfaces\LoggingObserver
     public function update(\SplSubject $message)
     {
         switch ($message->getLevel()) {
-            case $message::LEVEL_CRITICAL:    $level = \PEAR_LOG_EMERG;      break;
-            case $message::LEVEL_WARNING:     $level = \PEAR_LOG_CRIT;       break;
-            case $message::LEVEL_IMPORTANT:   $level = \PEAR_LOG_WARNING;    break;
-            case $message::LEVEL_DEBUGGING:   $level = \PEAR_LOG_DEBUG;      break;
-            case $message::LEVEL_INFORMATION: $level = \PEAR_LOG_INFO;       break;
-            default:                          $level = $message->getLevel(); break;
+        case $message::LEVEL_CRITICAL:
+            $level = \PEAR_LOG_EMERG;
+            break;
+        case $message::LEVEL_WARNING:
+            $level = \PEAR_LOG_CRIT;
+            break;
+        case $message::LEVEL_IMPORTANT:
+            $level = \PEAR_LOG_WARNING;
+            break;
+        case $message::LEVEL_DEBUGGING:
+            $level = \PEAR_LOG_DEBUG;
+            break;
+        case $message::LEVEL_INFORMATION:
+            $level = \PEAR_LOG_INFO;
+            break;
+        default:
+            $level = $message->getLevel();
+            break;
         }
         $this->_logger->log($message->getMessage(), $level);
     }
