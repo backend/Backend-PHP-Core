4c3e7a8c6edc4874e640cbcdaf73bbf8171c49c6
Jurgens du Toit <jrgns@jrgns.net>
2012-02-16 06:48:29 +0200

    Added Status Code Text and headers for HTTP and Application

--- Response.php
+++ Response.php
@@ -41,7 +41,71 @@ class Response
      * @var int The HTTP response code
      */
     protected $_status  = 200;
+    
+    /**
+     * @var string The HTTP version
+     */
+    protected $_http_version = 'HTTP/1.1';
 
+    /**
+     * @var array A list of HTTP Response Codes with their default texts
+     *
+     * Copied from the Zend_Http_Response object
+     */
+    protected static $messages = array(
+        // Informational 1xx
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+
+        // Success 2xx
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+
+        // Redirection 3xx
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',  // 1.1
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        // 306 is deprecated but reserved
+        307 => 'Temporary Redirect',
+
+        // Client Error 4xx
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+
+        // Server Error 5xx
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        509 => 'Bandwidth Limit Exceeded'
+    );
+    
     /**
      * @var array An associative array containing headers to be sent along with the response
      */
@@ -109,6 +173,11 @@ class Response
         if (headers_sent($file, $line)) {
             throw new \Exception('Headers already sent in ' . $file . ', line ' . $line);
         }
+        //Always send the HTTP status header first
+        header($this->_http_version . ' ' . $this->_status . ' ' . $this->getStatusText($this->_status));
+        if (!array_key_exists('X-Application', $this->_headers)) {
+            header('X-Application: Backend-PHP (Core)');
+        }
         foreach ($this->_headers as $name => $content) {
             header($name . ': ' . $content);
         }
@@ -117,6 +186,14 @@ class Response
     public function sendContent()
     {
         echo implode(PHP_EOL, $this->_content);
+    }
+    
+    public function getStatusText($code)
+    {
+        if (array_key_exists($code, self::$messages)) {
+            return self::$messages[$code];
+        }
+        return 'Unknown Status';
     }
 
     public function __toString()
