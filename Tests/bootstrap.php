35ed8da17be962f5256c14b6648dc0f974caf538
Jurgens du Toit <jurgens@everlytic.com>
2012-08-28 14:57:30 +0200

    Added test bootstrap to correct package [skip ci]

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -5,7 +5,7 @@
  * PHP Version 5.3
  *
  * @category  Backend
- * @package   Core
+ * @package   CoreTests
  * @author    J Jurgens du Toit <jrgns@backend-php.net>
  * @copyright 2011 - 2012 Jade IT (cc)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
